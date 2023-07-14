2842aa2285da3e6f3435e67c6b96598bb6c15c7c
harshal <harshal80503@gmail.com>
2023-07-14 12:49:48 +0530

    Updates

---
+++ precache-manifest.ba906944dbe697a749da9d8b63558ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cec6540dda02fa32735",
+    "url": "/my-app/static/css/main.3fb9f1ba.chunk.css"
+  },
+  {
+    "revision": "5cec6540dda02fa32735",
+    "url": "/my-app/static/js/main.5cec6540.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "c455ca1688bbfed041d73a73875a6069",
+    "url": "/my-app/index.html"
+  }
+];
